20ae8a948c5ae449cdafb05b44032b762ddb1293
Jeremy Taraba <43662755+JeremyTaraba@users.noreply.github.com>
2022-07-19 23:09:19 -0700

    Add files via upload

---
+++ coin_info.py
@@ -0,0 +1,17 @@
+# coin_info.py
+import data_retrieval
+
+class CoinInfo:
+  def __init__(self, name):
+    self.name = name
+    self.price = 0
+    self.data = data_retrieval.retrieve_data(name)
+
+
+  def retrieve_price(self):
+    self.price = data_retrieval.retrieve_price(self.name)
+    return self.price
+
+  
+  def retrieve_important_information(self):
+    data_retrieval.retrieve_additional_information(self.name)
---
+++ data_retrieval.py
@@ -0,0 +1,24 @@
+# data_retrieval.py
+import requests
+
+def retrieve_data(coin_name):
+  url_name = "https://data.messari.io/api/v1/assets/" + coin_name + "/metrics"
+  response = requests.get(url_name)
+  data = response.json()
+  
+  return data["data"]
+
+
+def retrieve_price(coin_name):
+  data = retrieve_data(coin_name)
+  price = (data['market_data']['price_usd'])
+  return price
+
+
+def retrieve_additional_information(coin_name):
+  data = retrieve_data(coin_name)
+  data_high = f'${data["market_data"]["ohlcv_last_24_hour"]["high"]}'
+  data_low = f'${data["market_data"]["ohlcv_last_24_hour"]["low"]}'
+  print('\n' + coin_name, "is ranked number", data["marketcap"]["rank"], "in the world.")
+  print("The highest price of", coin_name, "from the last 24 hours is", data_high)
+  print("The lowest price of", coin_name, "from the last 24 hours is", data_low)
