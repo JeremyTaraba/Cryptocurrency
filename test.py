81989ed95d96c58c2184eadca9548cbafde71fae
Jeremy Taraba <jeremytaraba@gmail.com>
2022-09-01 23:08:28 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ import requests
 url_name = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd"
 response = requests.get(url_name)
 data = response.json()
-print(data[0]["id"])
+print(data[0]["id"]) 
